6a0634edba5d0d2dd352659f219de27ed67cfdfe
Sai <sai.sglu@gmail.com>
2020-02-10 02:12:20 -0500

    "demo" REST API

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,96 @@
+package com.example.demo.controller;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Get all users list
+     *
+     * @return the users list
+     */
+    @GetMapping("/users")
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Gets users by id
+     *
+     * @param userId the user id
+     * @return the users by id
+     * @throws ResourceNotFoundException error
+     */
+    @GetMapping("users/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(" User not found on :: " + userId));
+        return ResponseEntity.ok().body(user);
+    }
+
+    /**
+     * Create user User
+     *
+     * @param user created
+     * @return the user
+     */
+    @PostMapping("/users")
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+
+    /**
+     * Update user response entity
+     *
+     * @param userId      the user id
+     * @param userDetails the user details
+     * @return the response entity
+     * @throws ResourceNotFoundException error
+     */
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails) throws ResourceNotFoundException {
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(" User not found on ::" + userId));
+        user.setEmail(userDetails.getEmail());
+        user.setLastName(userDetails.getLastName());
+        user.setFirstName(userDetails.getFirstName());
+        user.setUpdatedAt(new Date());
+        final User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    /**
+     * Delete user map
+     *
+     * @param userId the user id
+     * @return the map
+     * @throws Exception error
+     */
+    @RequestMapping("/users/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(" User not found on ::" + userId));
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @Column(name = "updated_by", nullable = false)
+    @LastModifiedBy
+    private String updatedBy;
+}
